47ce64f2264756e55c3caf684316d5de72aa680e
Egorka <eshik2002@gmail.com>
2022-03-21 19:41:20 +0300

    In

--- main.py
+++ main.py
@@ -5,23 +5,32 @@ from helper import DecisionHelper
 if __name__ == '__main__':
     weights = [0.09,  # Уровень производительности
                0.13,  # Совместимость
-               0.2,  # Удобство использования
+               0.04,  # Удобство использования
                0.05,  # Модифицируемость
                0.1,  # Эстетика пользовательского интерфейса
-               0.16,  # Функциональная полнота
+               0.4,  # Функциональная полнота
                0.06,  # Изучаемость
                0.03]  # Используемость ресурсов
 
-    print("Введите количество продуктов:")
-    n = int(input())
-    m = [[] for y in range(n)]
-    for i in range(n):
-        print("Продукт", i)
-        for j in range(len(weights)):
-            m[i].append(int(input()))
-        print("\n")
+    # print("Введите количество продуктов:")
+    # n = int(input())
+    # m = [[] for y in range(n)]
+    # for i in range(n):
+    #     print("Продукт", i)
+    #     for j in range(len(weights)):
+    #         m[i].append(int(input()))
+    #     print("\n")
+
+    m = np.array([[3, 2, 3, 2, 1, 2, 3, 2],  # Blender
+                  [2, 1, 3, 2, 1, 3, 1, 3],  # Zbrush
+                  [1, 2, 1, 2, 3, 3, 1, 3, ], # Cinema 4D
+                  [3, 1, 2, 1, 3, 2, 1, 2]])  # Adobe dimension
 
     dh = DecisionHelper(m, weights)
     print(dh.saw())
     print(dh.topsis())
-    print(dh.electre())
+    ar = dh.electre()
+    for a in ar:
+        for b in a:
+            print(b, end=" ")
+        print('\n')
